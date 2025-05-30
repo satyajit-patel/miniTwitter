740f974e71506df87398dc448ae3c512991f1796
satyajit-patel <satyajitpatel770001@gmail.com>
2025-05-30 15:55:57 +0530

    added login required using decorators

--- miniTwitter/tweet/views.py
+++ miniTwitter/tweet/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Tweet
 from .forms import TweetForm
 from django.shortcuts import get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -11,6 +12,7 @@ def tweetList(request):
     return render(request, 'tweetList.html', {'tweets': tweets})
 
 # create
+@login_required
 def tweetCreate(request):
     if request.method == 'POST':
         form = TweetForm(request.POST, request.FILES) # request.FILES if you are accepting files either
@@ -26,6 +28,7 @@ def tweetCreate(request):
     return render(request, 'tweetForm.html', {'form': form}) # render(request, whereToRender, dataSend)
 
 # edit
+@login_required
 def tweetEdit(request, tweetId):
     tweet = get_object_or_404(Tweet, pk=tweetId, user=request.user) # to only get to edit your own tweet
     if request.method == 'POST':
@@ -40,6 +43,7 @@ def tweetEdit(request, tweetId):
 
     return render(request, 'tweetForm.html', {'form': form})
 
+@login_required
 def tweetDelete(request, tweetId):
     tweet = get_object_or_404(Tweet, pk=tweetId, user=request.user)
     
